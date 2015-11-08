9ab238f1591a795e2f33a366984072d19579a089
artman202 <eti.strydom@gmail.com>
2015-11-08 12:28:11 +0200

    initial commit

---
+++ platforms/android/assets/www/scripts/controllers.js
@@ -0,0 +1,112 @@
+var app = angular.module('starter.controllers', [])
+
+app.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  $scope.navTitle='<img class="title-image" src="images/logo.svg" />';
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+app.controller('BookController', function($scope, $http, $timeout, $ionicLoading) {
+
+  $ionicLoading.show({
+    content: 'Loading',
+    animation: 'fade-in',
+    showBackdrop: true,
+    maxWidth: 200,
+    showDelay: 0
+  });
+
+  $timeout(function () {
+    $ionicLoading.hide();
+  }, 1000);
+
+  $scope.books = [];
+  var object = JSON.parse(sessionStorage.getItem("item"));
+  this.currentItem = object;
+
+  $http.get('http://criative.co.za/library-web-app-api/api.php/books').success(function(data) {
+  // $http.get('http://127.0.0.1/library-web-app-api/api.php/books').success(function(data) {
+    $scope.books = data;
+  });
+
+  this.setItem = function(item){
+    sessionStorage.setItem("item", JSON.stringify(item));
+  };
+
+})
+
+app.controller('ArticleController', function($scope, $http, $timeout, $ionicLoading) {
+
+  $ionicLoading.show({
+    content: 'Loading',
+    animation: 'fade-in',
+    showBackdrop: true,
+    maxWidth: 200,
+    showDelay: 0
+  });
+
+  $timeout(function () {
+    $ionicLoading.hide();
+  }, 1000);
+
+  $scope.articles = [];
+  var object = JSON.parse(sessionStorage.getItem("item"));
+  this.currentItem = object;
+
+  $http.get('http://criative.co.za/library-web-app-api/api.php/articles').success(function(data) {
+  // $http.get('http://127.0.0.1/library-web-app-api/api.php/articles').success(function(data) {
+    $scope.articles = data;
+  });
+
+  this.setItem = function(item){
+    sessionStorage.setItem("item", JSON.stringify(item));
+  };
+
+})
+
+app.controller('SearchController', function($scope, $http) {
+
+  // $scope.articles = [];
+  // var object = JSON.parse(sessionStorage.getItem("item"));
+  // this.currentItem = object;
+
+  // // $http.get('http://criative.co.za/library-web-app-api/api.php/books').success(function(data) {
+  // $http.get('http://127.0.0.1/library-web-app-api/api.php/articles').success(function(data) {
+  //   $scope.articles = data;
+  // });
+
+  // this.setItem = function(item){
+  //   sessionStorage.setItem("item", JSON.stringify(item));
+  // };
+
+})
